e90431ccae9f1be0ab0f3fd7edff62a6f29ddc80
janhaviphale267 <janhaviphale07@gmail.com>
2025-12-21 15:29:30 +0530

    app(context): wrap app with LoanProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
 import Header from "../components/Header";
 import UserDashboard from "../components/UserDashboard";
 import MainChat from "../components/MainChat";
+import { LoanProvider } from "../hooks/LoanContext";
 
 export default function App() {
   return (
-    <div className="h-screen flex flex-col bg-gray-50">
-      {/* HEADER */}
-      <Header />
-
-      {/* MAIN LAYOUT */}
-      <div className="flex flex-1 overflow-hidden">
-        {/* LEFT SIDEBAR */}
-        <div className="hidden md:block">
-          <UserDashboard />
+    <LoanProvider>
+      <div className="h-screen flex flex-col bg-gray-50">
+        <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <div className="hidden md:block">
+            <UserDashboard />
+          </div>
+          <main className="flex-1 overflow-hidden">
+            <MainChat />
+          </main>
         </div>
-
-        {/* CHAT AREA */}
-        <main className="flex-1 overflow-hidden">
-          <MainChat />
-        </main>
       </div>
-    </div>
+    </LoanProvider>
   );
 }
