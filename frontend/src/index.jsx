71a004fbc9827b18fa531ead95b3ffbb75ba3bff
janhaviphale267 <janhaviphale07@gmail.com>
2025-12-21 15:10:35 +0530

    ui(entry): stabilize index entry point and global styles import

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "../styles.css";
 
-const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-createRoot(rootElement).render(
-  <BrowserRouter>
+root.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
